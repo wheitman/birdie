cce94cf3961bd7aa4a99a3f913c6d7fa3283f482
BuildTools <unconfigured@null.spigotmc.org>
2018-06-28 15:46:23 -0500

    Created simple QML interaction within WeatherAlert.py, main.qml

--- canary/WeatherAlert.py
+++ canary/WeatherAlert.py
@@ -14,6 +14,9 @@ last edited: January 2015
 """
 
 import os, sys
+
+from PyQt5.QtCore import QUrl
+from PyQt5.QtQuick import QQuickView
 from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit,
                              QTextEdit, QGridLayout, QApplication, QPushButton, QSizePolicy)
 from PyQt5.QtGui import QPixmap
@@ -51,7 +54,7 @@ class CanaryAlert(QWidget):
 
         toggleFullscreen.clicked.connect(self.toggleFS)
 
-        #18 x 32 cells with 60px per cell
+        # 18 x 32 cells with 60px per cell
         self.setLayout(grid)
 
         banner = QWidget()
@@ -82,5 +85,16 @@ class CanaryAlert(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    alert = CanaryAlert()
-    sys.exit(app.exec_())
+    view = QQuickView()
+    view.setResizeMode(QQuickView.SizeRootObjectToView)
+    current_path = os.path.abspath(os.path.dirname(__file__))
+    qml_file = os.path.join(current_path, 'main.qml')
+    view.setSource(QUrl.fromLocalFile(qml_file))
+    if view.status() == QQuickView.Error:
+        sys.exit(-1)
+    view.show()
+    res = app.exec()
+    del view
+    sys.exit(res)
+    # alert = CanaryAlert()
+    # sys.exit(app.exec_())
